b22185d513c9d0bb2763cd3335cfe51a2eeb0d04
mamunurrashid08 <mamun1k999@gmail.com>
2025-08-16 00:49:00 +0400

    Update rocket.php

--- rocket.php
+++ rocket.php
@@ -6,7 +6,7 @@ $class_element_select = app_config('template')['form']['class_element_select'];
 
 $active = [
   'personal' => 'Personal',
-  'agent' => 'Agent',
+  'merchant' => 'Merchant',
   // 'merchant' => 'Merchant'
 ];
 
@@ -29,27 +29,28 @@ include 'common.php';
 
 <div class="">
   <div class="card-header">
-    <h3 class="card-title"><i class="fa-brands fa-square-reddit"></i> <?= lang("Rocket Setup for-" . $brand->brand_name) ?></h3>
+    <h3 class="card-title"><i class="fa-brands fa-square-reddit"></i> <?= lang("Pathao Pay Setup for-" . $brand->brand_name) ?></h3>
   </div>
   <div class="">
     <div class="">
       <?php echo form_open($form_url, $form_attributes); ?>
       <?php echo render_elements_form($general_elements); ?>
       <div id="personal<?= $brand->id ?>" class="type-class">
-        <label>Rocket Personal number</label>
-        <input type="text" name="personal_number" class="form-control" value="<?= @get_value($payment_settings->params, 'personal_number') ?>" placeholder="Enter your Rocket number">
+        <label>Pathao Pay Personal number</label>
+        <input type="text" name="personal_number" class="form-control" value="<?= @get_value($payment_settings->params, 'personal_number') ?>" placeholder="Enter your Pathao Pay number">
       </div>
-      <div id="agent<?= $brand->id ?>" class="type-class">
-        <label>Rocket Agent number</label>
-        <input type="text" name="agent_number" value="<?= @get_value($payment_settings->params, 'agent_number') ?>" class="form-control" placeholder="Enter your agent number">
+      <div id="merchant<?= $brand->id ?>" class="type-class">
+        <label>Pathao Pay Merchant number</label>
+        <input type="text" name="merchant_number" value="<?= @get_value($payment_settings->params, 'merchant_number') ?>" class="form-control" placeholder="Enter your Marchant number">
       </div>
-      <div id="merchant<?= $brand->id ?>" class="type-class d-none">
-        <label>Rocket Merchant Payment URL</label>
-        <input type="text" name="merchant_url" value="<?= @get_value($payment_settings->params, 'merchant_url') ?>" class="form-control" placeholder="Rocket Payment URL">
+      <div id="agent<?= $brand->id ?>" class="type-class d-none">
+        <label>Pathao Pay Merchant Payment URL</label>
+        <input type="text" name="agent_url" value="<?= @get_value($payment_settings->params, 'agent_url') ?>" class="form-control" placeholder="Pathao Pay Payment URL">
       </div>
-      <?= modal_buttons2('Save Rocket Setting', ''); ?>
+      <?= modal_buttons2('Save Pathao Pay Setting', ''); ?>
 
       <?php echo form_close(); ?>
     </div>
   </div>
+
 </div>
